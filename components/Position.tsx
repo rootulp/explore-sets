85551dd737e27affa17ae673f2494a62fc8dce36
Rootul Patel <rootulp@gmail.com>
2021-10-26 23:01:42 -0400

    Improve loading state

--- components/Position.tsx
+++ components/Position.tsx
@@ -1,8 +1,8 @@
-import { getExplorerAddressLink, shortenAddress } from "@usedapp/core";
+import { shortenAddress } from "@usedapp/core";
 import { useWeb3React } from "@web3-react/core";
 import { BigNumber, ethers } from "ethers";
 import React from "react";
-import { ListGroupItem } from "react-bootstrap";
+import { ListGroupItem, Placeholder } from "react-bootstrap";
 import { useCoingeckoToken } from "../lib/useCoingeckoToken";
 import styles from "../styles/Position.module.css"
 
@@ -13,13 +13,13 @@ interface PositionProps {
 
 export const Position = (props: PositionProps): JSX.Element => {
     const {chainId} = useWeb3React();
-    const { token, isLoading, isError } = useCoingeckoToken(props.address, chainId);
+    const { token, isLoading } = useCoingeckoToken(props.address, chainId);
 
     if (isLoading){
-        return (<ListGroupItem key={props.address}>Loading</ListGroupItem>)
-    }
-    if (isError){
-        return ( <ListGroupItem key={props.address}>Error</ListGroupItem>)
+        return (<ListGroupItem key={props.address}>
+            <Placeholder xs={6} animation="glow" />
+            <Placeholder xs={6} animation="glow" />
+        </ListGroupItem>)
     }
     return (
         <ListGroupItem key={props.address} className={styles.row}>
